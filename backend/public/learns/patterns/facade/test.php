124e187477249fc30c4976ad6961206e2f3197e4
xiewjsir <183778760@qq.com>
2019-01-16 10:41:35 +0800

    静谧风格个人博客模板

---
+++ backend/public/learns/patterns/facade/test.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * 结构型模式
+ *
+ * php外观模式
+ * 把系统中类的调用委托给一个单独的类，对外隐藏了内部的复杂性，很有依赖注入容器的感觉哦
+ *
+ * @author  TIGERB <https://github.com/TIGERB>
+ * @example 运行 php test.php
+ */
+
+
+// 注册自加载
+spl_autoload_register('autoload');
+
+function autoload($class)
+{
+    echo dirname($_SERVER['SCRIPT_FILENAME']) . '//..//' . str_replace('\\', '/', $class) . '.php'.PHP_EOL;
+    require dirname($_SERVER['SCRIPT_FILENAME']) . '//..//' . str_replace('\\', '/', $class) . '.php';
+}
+
+/************************************* test *************************************/
+
+use facade\AnimalMaker;
+
+// 初始化外观类
+$animalMaker = new AnimalMaker();
+
+// 生产一只猪
+$animalMaker->producePig();
+
+// 生产一只鸡
+$animalMaker->produceChicken();
